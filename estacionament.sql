0a2e94d5bfccb0778f9da6423891d1919899759d
Bruno Jokubauskas <brunojokubauskas@gmail.com>
2023-03-29 20:30:34 -0300

    path att

--- estacionament.sql
+++ estacionament.sql
@@ -32,8 +32,8 @@ CREATE TABLE
     FOREIGN KEY (matricula) REFERENCES funcionario (matricula)
   );
 
-LOAD DATA INFILE 'C:/Users/Aluno/Documents/bcd/veiculos.csv' INTO TABLE carro FIELDS TERMINATED BY ';' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 ROWS;
+LOAD DATA INFILE 'C:\Users\bruno\Desktop\BCD\veiculos.csv' INTO TABLE carro FIELDS TERMINATED BY ';' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 ROWS;
 
-LOAD DATA INFILE 'C:/Users/Aluno/Documents/bcd/funcionarios.csv' INTO TABLE funcionario FIELDS TERMINATED BY ';' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 ROWS;
+LOAD DATA INFILE 'C:\Users\bruno\Desktop\BCD\funcionarios.csv' INTO TABLE funcionario FIELDS TERMINATED BY ';' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 ROWS;
 
-LOAD DATA INFILE 'C:/Users/Aluno/Documents/bcd/manutencoes.csv' INTO TABLE manutencao FIELDS TERMINATED BY ';' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 ROWS;
+LOAD DATA INFILE 'C:\Users\bruno\Desktop\BCD\manutencoes.csv' INTO TABLE manutencao FIELDS TERMINATED BY ';' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 ROWS;
